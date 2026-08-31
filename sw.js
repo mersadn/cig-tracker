7742bb5aee8fd11652b8d3d3991296c5a17175e3
mersadn <mersad.n@gmail.com>
2026-08-31 07:43:18 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Cache-first app-shell service worker.
  * Bump CACHE_NAME on every deploy so users get updates. */
-const CACHE_NAME = 'cigtrack-v2';
+const CACHE_NAME = 'cigtrack-v3';
 
 const ASSETS = [
   './',
